21f3032fc8d78f4417e84126bdff081e0fb28e34
Nick Kinlen <nick_kinlen@hotmail.com>
2018-08-08 22:41:22 -0400

    First commit

---
+++ src/DrumContainer.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import MySlider from './Slider.js';
+import handle from './Slider.js';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import 'rc-tooltip/assets/bootstrap.css';
+import Tooltip from 'rc-tooltip';
+
+const DrumDisplay = (props) => {
+  return (
+    <div id="drum-machine">
+      <h1>React Drum Machine</h1>
+
+      <div className="drum-pad-container">
+        <div className="drum-pad">
+          <audio className="clip" id="Q">
+            <source src="#" type="audio/mp3" />
+          </audio>
+          <span>Q</span>
+        </div>
+        <div className="drum-pad">
+          <audio className="clip" id="W">
+            <source src="#" type="audio/mp3" />
+          </audio>
+          <span>W</span>
+        </div>
+        <div className="drum-pad">
+          <audio className="clip" id="E">
+            <source src="#" type="audio/mp3" />
+          </audio>
+          <span>E</span>
+        </div>
+        <div className="drum-pad">
+          <audio className="clip" id="A">
+            <source src="#" type="audio/mp3" />
+          </audio>
+          <span>A</span>
+        </div>
+        <div className="drum-pad">
+          <audio className="clip" id="S">
+            <source src="#" type="audio/mp3" />
+          </audio>
+          <span>S</span>
+        </div>
+        <div className="drum-pad">
+          <audio className="clip" id="D">
+            <source src="#" type="audio/mp3" />
+          </audio>
+          <span>D</span>
+        </div>
+        <div className="drum-pad">
+          <audio className="clip" id="Z">
+            <source src="#" type="audio/mp3" />
+          </audio>
+          <span>Z</span>
+        </div>
+        <div className="drum-pad">
+          <audio className="clip" id="X">
+            <source src="#" type="audio/mp3" />
+          </audio>
+          <span>X</span>
+        </div>
+        <div className="drum-pad">
+          <audio className="clip" id="C">
+            <source src="#" type="audio/mp3" />
+          </audio>
+          <span>C</span>
+        </div>
+    </div>
+
+
+      <div id="display">
+        <h3>Volume</h3>
+        <div className="slider">
+          <Slider min={0} max={20} defaultValue={3} handle={handle} />
+        </div>
+        <div className="label">
+          <span>Drum label goes here</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DrumDisplay;
---
+++ src/Slider.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import 'rc-tooltip/assets/bootstrap.css';
+import Tooltip from 'rc-tooltip';
+
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+//const Range = createSliderWithTooltip(Slider.Range);
+const Handle = Slider.Handle;
+
+const handle = (props) => {
+  const { value, dragging, index, ...restProps } = props;
+  return (
+    <Tooltip
+      prefixCls="rc-slider-tooltip"
+      overlay={value}
+      visible={dragging}
+      placement="top"
+      key={index}
+    >
+      <Handle value={value} {...restProps} />
+    </Tooltip>
+  );
+}
+
+
+
+export default handle;
